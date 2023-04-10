f191a13d5caad87da65331c5dc4c028840c174fc
Ania Misiorek <misiorek.a@northeastern.edu>
2023-04-09 20:57:32 -0400

    a9 stuff

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const API_BASE_A9 = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE_A9}/tuits`;
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit)
